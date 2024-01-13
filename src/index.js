3dbe5d85b2967bb29659ad11cd969e239bc336d6
autumn034 <l324018852@outlook.com>
2024-01-12 21:27:06 -0500

    Add CSS

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import css from "./style.css";
-async function getWeather(cityName) {
+async function getWeatherData(cityName) {
     try {
         const APIKey = "3cca2df03456fd625a7bbe7f00a970d4";
 
-        // grab coordinates from city name
+        // get coordinates from city name
         let geocode = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${APIKey}`, {mode: "cors"});
         geocode = await geocode.json();
         let lat = geocode[0]["lat"];
         let lon = geocode[0]["lon"];    
 
-        // grab weather data from coordinates
+        // get weather data from coordinates
         let weatherData = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${APIKey}`, {mode: "cors"});
         weatherData = await weatherData.json();
-        console.log(weatherData);
+        return weatherData;
 
     } catch (error) {
         console.log(error);
     }
 };
 
-getWeather("Toronto")
+getWeatherData("Toronto")
